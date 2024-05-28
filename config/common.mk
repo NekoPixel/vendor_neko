af5f85f0945089e8a2802661fb8ad3b41f95b0ca
Dmitrii <bankersenator@gmail.com>
2024-05-28 06:26:20 +0000

    vendor: enable gapps flags by default
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # GMS
 $(call inherit-product, vendor/google/gms/products/gms.mk)
 
+WITH_GAPPS := true
+TARGET_INCLUDE_PIXEL_FRAMEWORKS := true
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
